2f67805e850eb533a90325b6a0f12657c376dc74
Harshada Patil <168923120+HarshadaPatilS@users.noreply.github.com>
2024-10-20 12:27:23 +0530

    Delete e-learning-system directory

--- e-learning-system/src/main/java/com/example/elearning/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.elearning.repository;
-
-import com.example.elearning.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
