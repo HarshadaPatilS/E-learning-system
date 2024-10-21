00f4e9ccba14f9ff8b8a9d6a4a9aaa8efb01c0d3
Harshada Patil <168923120+HarshadaPatilS@users.noreply.github.com>
2024-10-21 18:04:07 +0000

    Initial commit with all project files

---
+++ e-learning-system/src/main/java/com/example/elearning/ElearningSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElearningSystemApplication.class, args);
+    }
+}
---
+++ e-learning-system/src/main/java/com/example/elearning/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.example.elearning.controller;
+
+import com.example.elearning.model.Course;
+import com.example.elearning.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+}
